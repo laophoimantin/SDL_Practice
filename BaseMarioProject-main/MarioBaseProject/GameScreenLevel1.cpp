39df6dcb826767418ef7a7e6fcb4163f14f2d24b
laophoimantin <142765802+laophoimantin@users.noreply.github.com>
2024-05-04 10:55:21 +0700

    Waaaaa

---
+++ BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.cpp
@@ -0,0 +1,17 @@
+#include "GameScreenLevel1.h"
+#include "Texture2D.h"
+#include <iostream>
+
+GameScreenLevel1::GameScreenLevel1(SDL_Renderer* renderer) : GameScreen(renderer) {
+
+	SetUpLevel();
+}
+GameScreenLevel1::~GameScreenLevel1() {
+
+}
+void GameScreenLevel1::Render() {
+
+}
+void GameScreenLevel1::Update(float deltaTime, SDL_Event e) {
+
+}
---
+++ BaseMarioProject-main/MarioBaseProject/GameScreenLevel1.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef _GAMESCREENLEVER1_H
+#define _GAMESCREENLEVER1_H
+
+#include "Commons.h"
+#include "GameScreen.h"
+
+class Texture2D;
+
+class GameScreenLevel1
+{
+	
+};
+
+class GameScreenLevel1 : GameScreen {
+
+public:
+
+	GameScreenLevel1(SDL_Renderer* renderer);
+	~GameScreenLevel1();
+
+	void Render() override;
+	void Update(float deltaTime, SDL_Event e) override;
+
+
+protected:
+
+private:
+	Texture2D* m_background_texture;
+	bool SetUpLevel();
+
+};
+
+
+#endif 
